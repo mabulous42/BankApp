f0b9c87fdac3055d61ae744a69e144c4bdc67f71
Abbas <tundeeey@gmail.com>
2023-05-18 01:47:10 +0100

    changed the signup btn text color from black to white when the check box is checked and also return to black when the check box is unchecked

--- SignIn/SignUp/signup.js
+++ SignIn/SignUp/signup.js
@@ -1,5 +1,6 @@
 let signUpBtn = document.getElementById("signUp-btn");
 let check = document.getElementById("check-box");
+let signUpText = document.getElementById("sign-up-id");
 
 //this function navigates from the signup page to signin page
 function gotoSignIn() {
@@ -12,9 +13,11 @@ function checkBox() {
     if (check.checked == true) {
         signUpBtn.disabled = false;
         signUpBtn.style.background = "rgba(0, 168, 89, 1)";
+        signUpText.style.color = "white";
     } else {
         signUpBtn.disabled = true;
         signUpBtn.style.background = "rgba(217, 217, 217, 1)";
+        signUpText.style.color = "black";
     }
 }
 
