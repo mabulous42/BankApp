3a46ad5136ebfd17be9479eec4e7bde176c4aeb7
Abbas <tundeeey@gmail.com>
2023-05-22 12:06:57 +0100

    done with the function that displays the generated account number after the laoder has loaded for few seconds

---
+++ Generating/generating.js
@@ -0,0 +1,7 @@
+let accountGenerating = document.getElementById("generating");
+
+setTimeout(() => {
+    window.location.href = "../AccountGenerated/accountGenerated.html"
+}, 5000);
+
+// window.location.href = "../AccountGenerated/accountGenerated.html";
