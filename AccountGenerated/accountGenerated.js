09d9b82b6ce09c04a072d573c6061523378eb58a
Abbas <tundeeey@gmail.com>
2023-05-22 14:11:41 +0100

    i modified the generated number to have min and max number so that the generated account number can be a unique number

--- AccountGenerated/accountGenerated.js
+++ AccountGenerated/accountGenerated.js
@@ -4,17 +4,16 @@ let registeredCustomer = JSON.parse(localStorage.getItem('customers'));
 // registeredCustomer.accountNumber = 1123;
 
 registeredCustomer.forEach(element => {
-    element.accountNumber = Math.floor(Math.random() * 10000000000);
+    element.accountNumber = Math.floor(Math.random() * (3030999999 - 3030000000 + 1)) + 3030000000;
     console.log(element.firstName);
     document.getElementById('account-number').innerHTML = element.accountNumber;
-
 });
 
-localStorage.setItem('customer', JSON.stringify(registeredCustomer));
 
 console.log(registeredCustomer);
 
 
 function gotoSignIn() {
+    localStorage.setItem('customer', JSON.stringify(registeredCustomer));
     window.location.href = "../Signin/signin.html";
 }
