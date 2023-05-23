dbbee6b48f3dbf8e845347ead8532aae429da338
Abbas <tundeeey@gmail.com>
2023-05-23 08:35:53 +0100

    I added a condition to the continue button on the account number generate to check if the account number has not been assigned to a customer before, else it should generate another one before finally adding it to the customer;s details and saving into local storage

--- AccountGenerated/accountGenerated.js
+++ AccountGenerated/accountGenerated.js
@@ -1,22 +1,29 @@
 //fetching the registered customers from the local storage
 let registeredCustomer = JSON.parse(localStorage.getItem('customers'));
 
-let currentRegisteredCustomer = registeredCustomer.length-1;
+let currentRegisteredCustomer = registeredCustomer.length - 1;
 
-//setting the index of the current registered customers to update their data by adding account number to their data
-registeredCustomer[currentRegisteredCustomer].accountNumber = Math.floor(Math.random() * (3030999999 - 3030000000 + 1)) + 3030000000;
-
-//passing the account number of the current registered customer to a new varaible
-let accountNo = registeredCustomer[currentRegisteredCustomer].accountNumber;
-
-//displaying the account number for the user to see so it can be noted down
-document.getElementById('account-number').innerHTML = accountNo;
+let accountDigit;
+function generateAccount() {
+    accountDigit = Math.floor(Math.random() * (3030999999 - 3030000000 + 1)) + 3030000000;
+    //displaying the account number for the user to see so it can be noted down
+    document.getElementById('account-number').innerHTML = accountDigit;
+}
+generateAccount();
 
 //showing all the registered customer on console
 console.log(registeredCustomer);
 
 //this function saves the modified customer data back to the local storage and then proceed to the next page
 function gotoSignIn() {
-    localStorage.setItem('customers', JSON.stringify(registeredCustomer));
-    window.location.href = "../TransactionPin/transactionPin.html";
+    let found = registeredCustomer.find(acc => acc.accountNumber == accountDigit);
+    if (found) {
+        generateAccount();
+    } else {
+        console.log("Available for use");
+        //setting the index of the current registered customers to update their data by adding account number to their data
+        registeredCustomer[currentRegisteredCustomer].accountNumber = accountDigit;
+        localStorage.setItem('customers', JSON.stringify(registeredCustomer));
+        window.location.href = "../TransactionPin/transactionPin.html";
+    }
 }
