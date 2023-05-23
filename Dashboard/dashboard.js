de5043b0b84907f38bebea26e36f8eef72f9054e
Abbas <tundeeey@gmail.com>
2023-05-23 22:14:15 +0100

    added account number to the dashboard

--- Dashboard/dashboard.js
+++ Dashboard/dashboard.js
@@ -16,6 +16,9 @@ console.log(customerName);
 //displaying the current customer first name on the dashboard
 document.getElementById("customer-name").innerHTML = customerName;
 
+//displaying the current customer account number
+document.getElementById("bankEase-account").innerHTML = currentUser.accountNumber;
+
 let displayBalance = document.getElementById("show-bal");
 let hide_Balance = document.getElementById("hide-bal");
 let currentBalance = document.getElementById("current-balance");
