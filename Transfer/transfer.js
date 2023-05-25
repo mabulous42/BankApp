b1af216467e50543e31691a1e45a79851ee16189
Abbas <tundeeey@gmail.com>
2023-05-25 23:08:26 +0100

    added function to the keypad to display on the amount screen each time the keypads are pressed

--- Transfer/transfer.js
+++ Transfer/transfer.js
@@ -1,7 +1,27 @@
+let enterWithKeyPad = document.getElementById("enter-amount");
 function gotoDashboard() {
     window.location.href = "../Dashboard/dashboard.html";
 }
 
 function showTransferToBankEase() {
     document.getElementById("bankEaseTransfer").style.right = "0px";
+}
+
+function gotoTransfer() {
+    window.location.href = "transfer.html";
+}
+
+let transferAmount = document.getElementById("transfer-amount");
+let amount = 1000000;
+// transferAmount.innerHTML = amount.toLocaleString();
+// transferAmount.innerHTML.toLocaleString();
+
+function displayNumber(num) {
+    enterWithKeyPad.style.display = "none";
+    transferAmount.innerHTML += Number(num).toLocaleString();
+    // transferAmount.innerHTML.toLocaleString();
+}
+
+function del() {
+    transferAmount.innerHTML.slice(0,-1);
 }
