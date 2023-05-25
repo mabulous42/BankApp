bc91ff1ac79df3d80fe649713101b3a7ff1416f9
Abbas <tundeeey@gmail.com>
2023-05-25 13:18:38 +0100

    added a div to test its movement transition when a button is clicked on

--- Transfer/transfer.js
+++ Transfer/transfer.js
@@ -1,3 +1,7 @@
 function gotoDashboard() {
     window.location.href = "../Dashboard/dashboard.html";
 }
+
+function showTransferToBankEase() {
+    document.getElementById("bankEaseTransfer").style.right = "0px";
+}
