169e7f6775211c801cc033cbef97c9c23ffdc665
Abbas <tundeeey@gmail.com>
2023-05-29 12:18:04 +0100

    added another div with smooth transition and opacity before the get started page is finally displayed

--- getStarted.js
+++ getStarted.js
@@ -5,14 +5,22 @@ function gotoSignInPage() {
 }
 
 setTimeout(() => {
-    document.getElementById("bankease-img").style.visibility = "visible";
+    document.getElementById("image-div-1").style.top = "10rem";
+    setTimeout(() => {        
+        document.getElementById("image-div-1").style.opacity = "0";
+        document.getElementById("image-div-1").style.visibility = "hidden";
+    }, 1000);
 }, 1000);
+
+setTimeout(() => {
+    document.getElementById("bankease-img").style.visibility = "visible";
+}, 2400);
 
 setTimeout(() => {
     document.getElementById("banking-made").style.visibility = "visible";
     document.getElementById("banking-made").style.top = "31.8rem";
-}, 3000);
+}, 4500);
 
 setTimeout(() => {
     document.getElementById("main").style.right = "0";
-}, 6000);
+}, 7500);
