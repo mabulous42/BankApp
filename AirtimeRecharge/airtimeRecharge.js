33290269041319215641301d00f80d47e2ca05ce
Abbas <tundeeey@gmail.com>
2023-05-30 21:34:19 +0100

    added a loader to the airtime page and also set the pay button disable to be true until an amount between 50 and 5000 is entered

--- AirtimeRecharge/airtimeRecharge.js
+++ AirtimeRecharge/airtimeRecharge.js
@@ -1,27 +1,49 @@
 let naira = document.getElementById("naira");
 let payBtn = document.getElementById("pay-btn");
 let inputAmount = document.getElementById("input-amount");
+let loader = document.getElementById("loader");
 let placeHolder = inputAmount.placeholder;
+
+loader.style.display = "none";
+payBtn.disabled = true;
+
+let currentUser = JSON.parse(localStorage.getItem('CU'));
+let allBankEaseUser = JSON.parse(localStorage.getItem('customers'));
+
+let currentUserIndex = allBankEaseUser.findIndex(user => user.accountNumber == currentUser.accountNumber);
+
 
 console.log(placeHolder);
 
 naira.style.display = "none"
 
 function showNaira() {
-    if (placeHolder == "") {
-        naira.style.display = "block";        
-    } else {
-        naira.style.display = "none";        
-    }
+    naira.style.display = "block";
 }
 
-payBtn.addEventListener('click', function(){
+let rechargeAount;
+payBtn.addEventListener('click', function () {
+    console.log(inputAmount.value);
+    loader.style.display = "block"
+    setTimeout(() => {
+        loader.style.display = "none"
+    }, 2000);
+
+
 })
 
-inputAmount.addEventListener('input', ()=>{
+inputAmount.addEventListener('input', () => {
     if (inputAmount.value.length != "") {
-        payBtn.style.backgroundColor = "rgba(0, 168, 89, 1)";
+        if (inputAmount.value >= 50 && inputAmount.value <= 5000) {
+            payBtn.style.backgroundColor = "rgba(0, 168, 89, 1)";
+            payBtn.disabled = false;
+        }
+        else {
+            payBtn.style.backgroundColor = "rgb(141, 216, 180)";
+            payBtn.disabled = true;
+        }
     } else {
         payBtn.style.backgroundColor = "rgb(141, 216, 180)";
-    }    
+        payBtn.disabled = true;
+    }
 })
