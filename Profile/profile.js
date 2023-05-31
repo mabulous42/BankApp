2fcef1f03bbfd06c94d4e05543bde71634a99516
Abbas <tundeeey@gmail.com>
2023-05-31 23:04:33 +0100

    displayed the current user full name on the profile page

--- Profile/profile.js
+++ Profile/profile.js
@@ -1,7 +1,13 @@
+let currentUser = JSON.parse(localStorage.getItem('CU'));
+
+document.getElementById("display-name").innerHTML = currentUser.firstName + " " + currentUser.lastName
+
+//the function navigates the user to the dashboard page
 function gotoDashboard() {
     window.location.href = "../Dashboard/dashboard.html";
 }
 
+//sign out function
 function signOut() {
     localStorage.removeItem('CU');
     setTimeout(() => {
