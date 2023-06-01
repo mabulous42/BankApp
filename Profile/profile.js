8fda3b49daefa105aecb7a90cbea1f6b5d01a3cc
Abbas <tundeeey@gmail.com>
2023-06-01 23:52:33 +0100

    added a fuction to upload and save image then created a new field in the users local storage to ave the picture url as the profilePicture

--- Profile/profile.js
+++ Profile/profile.js
@@ -1,4 +1,8 @@
 let currentUser = JSON.parse(localStorage.getItem('CU'));
+let allBankEaseUser = JSON.parse(localStorage.getItem("customers"));
+
+let currentUserIndex = allBankEaseUser.findIndex(user => user.accountNumber == currentUser.accountNumber);
+console.log(currentUserIndex);
 
 document.getElementById("display-name").innerHTML = currentUser.firstName + " " + currentUser.lastName
 
@@ -14,3 +18,32 @@ function signOut() {
         window.location.href = "../Signin/signin.html";
     }, 2000);
 }
+
+function uploadImage(event) {
+    let file = event.target.files[0];
+    let reader = new FileReader();
+    console.log(file);
+
+    reader.addEventListener('load', (e) => {
+        console.log(e);
+        let imgUrl = e.target.result;
+        console.log(imgUrl);
+        currentUser.profilePicture = imgUrl;
+        allBankEaseUser[currentUserIndex].profilePicture = currentUser.profilePicture;
+        console.log(allBankEaseUser[currentUserIndex].profilePicture);
+        localStorage.setItem("CU", JSON.stringify(currentUser));
+        localStorage.setItem("customers", JSON.stringify(allBankEaseUser));
+    })
+
+    if (file) {
+        reader.readAsDataURL(file)
+    }
+}
+
+function displayProfileImage() {
+    document.getElementById("profileImageTag").innerHTML = `
+    <img src="${currentUser.profilePicture}" alt="" class="p-image-div rounded-circle">
+    `
+}
+
+displayProfileImage();
